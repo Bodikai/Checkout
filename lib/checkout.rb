3a632d75ac8d3e8f28dc989f01f3802f52efb608
Bodikai <omar.salleh@gmail.com>
2016-12-21 10:17:10 +0000

    New OFFERS class for better SR.

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,39 +1,39 @@
+class Offers
+  def base_price(item)
+    @rules[item][0]
+  end
+
+  def offer_price(rules, items, item, occurrence)
+    @rules = rules
+    if offer_type(item) == "buy_one_get_one" && occurrence % 2 == 0
+      return 0
+    elsif offer_type(item) == "bulk_discount" && items.count(item) >= @rules[item][2]
+      return @rules[item][3]
+    end
+    base_price(item)
+  end
+
+  def offer_type(item)
+    @rules.fetch(item)[1]
+  end
+end
+
 class PricingRules
   def initialize(rules)
     @rules = rules
     @currency = @rules["Currency"]
   end
 
-  def base_price(item)
-    @rules[item][0]
-  end
-
-  def buy_one_get_one?(item, occurrence)
-    offer_type(item) == "buy_one_get_one" && occurrence % 2 == 0
-  end
-
   def currency
     @currency
   end
 
-  def bulk_discount?(items, item)
-    offer_type(item) == "bulk_discount" && items.count(item) >= @rules[item][2]
-  end
-
-  def offer_type(item)
-    @rules.fetch(item)[1]
+  def offers
+    Offers.new
   end
 
   def price(items, item, occurrence)
-    if buy_one_get_one?(item, occurrence)
-      # puts "#{item} 1 x #{currency}#{'%.02f' % 0} Buy one get one free"
-      return 0
-    elsif bulk_discount?(items, item)
-      # puts "#{item} 1 x #{currency}#{'%.02f' % @rules[item][3]} Bulk discount for #{@rules[item][2]} or more"
-      return @rules[item][3]
-    end
-    # puts "#{item} 1 x #{currency}#{'%.02f' % base_price(item)}"
-    base_price(item)
+    offers.offer_price(@rules, items, item, occurrence)
   end
 end
 
